888399b2d1df94c5876fdf45da3b0f5426bc2700
MaxiEliteExecutor <maxielitecheat@gmail.com>
2025-04-18 12:04:26 +0200

    s

--- main.lua
+++ main.lua
@@ -10,5 +10,17 @@ function GetSpooferVersion()
     return "V1.0"
 end
 
+function uncTest()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/unified-naming-convention/NamingStandard/main/UNCCheckEnv.lua", true))()
+end
 
+local old_identify
+old_identify = hookfunction(identifyexecutor, function()
+    print("identifyexecutor() called!")
+    return "ARC", "1.0.0"
+end)
 
+local old_getname
+old_getname = hookfunction(getexecutorname, function()
+    return "ARC" -- Fake executor name
+end)
