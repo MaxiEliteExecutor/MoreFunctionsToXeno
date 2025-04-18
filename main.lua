6c12d4dca928f77064c289b2ce629a3e7ad561ad
MaxiEliteExecutor <maxielitecheat@gmail.com>
2025-04-18 11:42:35 +0200

    ss

--- main.lua
+++ main.lua
@@ -2,8 +2,8 @@ function infyeild()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
 end
 
-function GetAPIDetails()
-    return "ARC Xeno API V1.0 Spoofer" 
+function GetSpooferDetails()
+    return "ARC Xeno API V1.0 " 
 end
 
 
