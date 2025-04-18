0981b5c1e9208dc34c218dec81ea1198b79e3f3f
MaxiEliteExecutor <maxielitecheat@gmail.com>
2025-04-18 13:00:49 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -17,21 +17,7 @@ old_getname = hookfunction(getexecutorname, function()
 end)
 
 -- Hook the 'request' function to return a fake response
-local HttpService = game:GetService("HttpService")
 
-hookfunction(request, function(args)
-    if typeof(args) == "table" and args.Url == "https://httpbin.org/user-agent" then
-        return {
-            StatusCode = 200,
-            Body = HttpService:JSONEncode({
-                ["user-agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; ExploitClient)"
-            })
-        }
-    end
-
-    -- fallback to original request if different URL (optional)
-    return getgenv().originalRequest and getgenv().originalRequest(args)
-end)
 
 -- Optional: save original request if needed later
 getgenv().originalRequest = request
@@ -40,7 +26,7 @@ function OwlHub()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/CriShoux/OwlHub/master/OwlHub.txt"))();
 end
 
-function Vulnerability()
+--[[function Vulnerability()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/aihkw/exe-test/refs/heads/main/executor_vuln_test.lua"))();
 end
 --[[local function ARCGUILib()
